d116cc9cddcaff25333d08a468d81d2b2683c1a2
Binye Barwe <binyebarwe@gmail.com>
2022-10-22 17:28:28 -0400

    fix addresses

--- tests/mango-reimbursement/reimbursement.ts
+++ tests/mango-reimbursement/reimbursement.ts
@@ -19,39 +19,39 @@ describe("mango reimbursement", () => {
 
   const program = client.BestApy;
 
-  xit("Mango reimbursement USDC", async () => {
+  it("Mango reimbursement USDC", async () => {
     program.setToken("USDC");
     let tx = await program.mangoReimbursement(15, new BN(3529));
     await program.provider.sendAndConfirm(tx, [], CONFIRM_OPTS);
   });
 
-  xit("Mango reimbursement WSOL", async () => {
+  it("Mango reimbursement WSOL", async () => {
     program.setToken("WSOL");
     let tx = await program.mangoReimbursement(3, new BN(2002));
     await program.provider.sendAndConfirm(tx, [], CONFIRM_OPTS);
   });
 
-  xit("Mango reimbursement MNGO", async () => {
+  it("Mango reimbursement MNGO", async () => {
     program.setToken("MNGO");
     let tx = await program.mangoReimbursement(0, new BN(12090));
     await program.provider.sendAndConfirm(tx, [], CONFIRM_OPTS);
   });
 
-  xit("Mango reimbursement RAY", async () => {
+  it("Mango reimbursement RAY", async () => {
     program.setToken("RAY");
-    let tx = await program.mangoReimbursement(6, new BN(0));
+    let tx = await program.mangoReimbursement(6, new BN(15826)); // 16587
     await program.provider.sendAndConfirm(tx, [], CONFIRM_OPTS);
   });
 
-  xit("Mango reimbursement mSOL", async () => {
+  it("Mango reimbursement mSOL", async () => {
     program.setToken("mSOL");
-    let tx = await program.mangoReimbursement(10, new BN(0));
+    let tx = await program.mangoReimbursement(10, new BN(18614)); //19629
     await program.provider.sendAndConfirm(tx, [], CONFIRM_OPTS);
   });
 
   it("Mango reimbursement USDT", async () => {
     program.setToken("USDT");
-    let tx = await program.mangoReimbursement(4, new BN(0));
+    let tx = await program.mangoReimbursement(4, new BN(20158)); //21320
     await program.provider.sendAndConfirm(tx, [], CONFIRM_OPTS);
   });
 });
